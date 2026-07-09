617110975d0a0c1b31fe1bdd9883f2674c51de21
prachirajpurohit <248518054+prachirajpurohit@users.noreply.github.com>
2026-07-09 23:58:00 +0530

    add statusbar

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
+import { StatusBar } from "expo-status-bar";
 import Main from "./src/components/Main";
 
 const App = () => {
-  return <Main />;
+  return (
+    <>
+      <StatusBar style="light" />
+      <Main />
+    </>
+  );
 };
 
 export default App;
