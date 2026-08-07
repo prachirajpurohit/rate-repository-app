f0a9f733d53b1b0d710c71dc1b86cc9e5c07d823
prachirajpurohit <248518054+prachirajpurohit@users.noreply.github.com>
2026-08-08 00:13:55 +0530

    add auth token to apollo client

--- App.js
+++ App.js
@@ -3,8 +3,10 @@ import { ApolloProvider } from "@apollo/client/react";
 import Main from "./src/components/Main";
 import { NativeRouter } from "react-router-native";
 import createApolloClient from "./src/utils/apolloClient";
+import AuthStorage from "./src/utils/authStorage";
 
-const apolloClient = createApolloClient();
+const authStorage = new AuthStorage();
+const apolloClient = createApolloClient(authStorage);
 
 const App = () => {
   console.log("env check:", process.env.EXPO_PUBLIC_ENV);
